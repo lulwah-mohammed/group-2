824414c3cbd4556327f80b653bf1c923ad5825b2
lulwah-mohammed <148808819+lulwah-mohammed@users.noreply.github.com>
2023-10-26 22:48:16 +0300

    Create OnlineClass.java

---
+++ OnlineClass.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class OnlineClass {
+    private String className;
+    private String teacherName;
+    private static List<String> students;
+    private Date date;
+    private int duration; // Duration of the class in minutes
+
+    public OnlineClass(String className, String teacherName, Date date, int duration) {
+        this.className = className;
+        this.teacherName = teacherName;
+        this.students = new ArrayList<>();
+        this.date = date;
+        this.duration = duration;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public List<String> getStudents() {
+        return students;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void addStudent(String studentName) {
+        students.add(studentName);
+    }
+
+    public static void removeStudent(String studentName) {
+        students.remove(studentName);
+        System.out.println(studentName+"is removed successfully");
+    }
+
+
+public static void main(String[] args) {
+    System.out.println("Enter a student name to kick him from the class");
+    Scanner s = new Scanner(System.in);
+    String nameToKick = s.nextLine();
+    removeStudent(nameToKick);
+
+}
+
+class Schedule {
+    private List<OnlineClass> classes = new ArrayList<>();
+
+    public void addClass(OnlineClass onlineClass) {
+        classes.add(onlineClass);
+    }
+
+    public List<OnlineClass> getClasses() {
+        return classes;
+    }
+}
+}
